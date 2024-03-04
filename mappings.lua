27a3e41eeb7a030db30f7ed35a6dbf257b0e7309
20sisca <alexis.siscaa@gmail.com>
2024-03-04 13:38:57 +0100

    Basic mappings, community plugins for python and typescript

--- mappings.lua
+++ mappings.lua
@@ -7,16 +7,22 @@ return {
   -- first key is the mode
   n = {
     -- second key is the lefthand side of the map
-
+    [":"] = {";"},
+    [";"] = {":"},
+    ["<bs>"] = { "<c-^>", desc = "Go to previous buffer" },
+    ["<C-d>"] = {"<C-d>zz"},
+    ["<C-u>"] = {"<C-u>zz"}, 
+    ["n"] = {'nzzzv'},
+    ["N"] = {'Nzzzv'},
     -- navigate buffer tabs with `H` and `L`
-    -- L = {
-    --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-    --   desc = "Next buffer",
-    -- },
-    -- H = {
-    --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-    --   desc = "Previous buffer",
-    -- },
+    L = {
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+      desc = "Next buffer",
+    },
+    H = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Previous buffer",
+    },
 
     -- mappings seen under group name "Buffer"
     ["<leader>bD"] = {
@@ -32,6 +38,12 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+  },
+  i = {      
+    -- ["kj"] = { "<esc>"},
+    ["JK"] = { "<esc>"},
+    -- ["JJ"] = { "<esc>"},
+    -- ["KJ"] = { "<esc>"},
   },
   t = {
     -- setting a mapping to false will disable it
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,7 +1,8 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
+  -- "jesseduffield/lazygit.nvim",
+ {"github/copilot.vim", lazy=false},
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
